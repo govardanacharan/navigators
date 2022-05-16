caa3e2fe1795b7acf986ad30aaa6862430f8e795
govardanacharan <95639773+govardanacharan@users.noreply.github.com>
2022-05-16 09:26:58 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Stacknavi from './components/stacknavi';
+import Tabnavi from './components/tabnavi';
+export default function App(){
+  return(
+    <><Stacknavi></Stacknavi><Tabnavi></Tabnavi></>
+  );
+}
